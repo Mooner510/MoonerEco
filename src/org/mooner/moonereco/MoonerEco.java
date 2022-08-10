4e1241be6fa0ecb1ddbe6a9eb10dd337a0590c0f
Mooner510 <lmck510@gmail.com>
2022-08-10 19:05:10 +0900

    Added BungeeAPI

--- src/org/mooner/moonereco/MoonerEco.java
+++ src/org/mooner/moonereco/MoonerEco.java
@@ -2,6 +2,7 @@ package org.mooner.moonereco;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.mooner.moonerbungeeapi.api.BungeeAPI;
 import org.mooner.moonereco.API.EcoAPI;
 import org.mooner.moonereco.API.LogData;
 import org.mooner.moonereco.API.LogType;
@@ -13,7 +14,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mooner.moonereco.hook.Bungee;
 import org.mooner.moonereco.hook.EcoHook;
 import org.mooner.moonereco.hook.EcoManager;
 
@@ -117,7 +117,7 @@ public class MoonerEco extends JavaPlugin implements Listener {
                                 if(offPlayer != null)
                                     offPlayer.sendMessage(prefix + chat("&e" + p.getName() + "&f님으로부터 &a" + parseString(d, 2, true) + "원&f을 받았습니다."));
                                 else
-                                    Bungee.sendBungeeMessage(player.getName(), prefix + chat("&e" + p.getName() + "&f님으로부터 &a" + parseString(d, 2, true) + "원&f을 받았습니다."));
+                                    BungeeAPI.sendBungeeMessage(player.getName(), prefix + chat("&e" + p.getName() + "&f님으로부터 &a" + parseString(d, 2, true) + "원&f을 받았습니다."));
                             } else {
                                 p.sendMessage(prefix + chat("&c보낼 금액을 입력해주세요."));
                             }
